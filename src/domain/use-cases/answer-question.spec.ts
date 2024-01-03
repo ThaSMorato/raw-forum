de6b00efaf2fcf759466de4f11aaef2de5df8069
ThaSMorato <tha.s.morato@gmail.com>
2024-01-03 00:57:29 -0300

    feat: initial use case and test

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,10 @@
+import {expect, it} from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+
+it('should create an answer', () => {
+  const answerQuestion = new AnswerQuestionUseCase()
+
+  const answer = answerQuestion.execute({content:'Nova Resposta', instructorId: '1', questionId: '1'})
+
+  expect(answer.content).toEqual('Nova Resposta')
+})
