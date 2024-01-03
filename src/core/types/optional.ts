e34bf94bab3bc3e2d17a91fb797c72f30b8c4b91
ThaSMorato <tha.s.morato@gmail.com>
2024-01-03 11:29:54 -0300

    feat: optional type

---
+++ src/core/types/optional.ts
@@ -0,0 +1,16 @@
+/**
+ * Make some property optional on type
+ *
+ * @example
+ * ```typescript
+ * type Post {
+ * id: string;
+ * name: string;
+ * age: number;
+ * }
+ *
+ * Optional<Post, 'id' | 'age'>
+ * ```
+ */
+
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T,K>
