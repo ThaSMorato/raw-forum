cc7cf71d4eddc1e3ce87b9082cfe63a5952b41b3
ThaSMorato <tha.s.morato@gmail.com>
2024-01-03 19:27:11 -0300

    test: returning functions

--- test/repositories/fake-repositories/fake-answer-repository.ts
+++ test/repositories/fake-repositories/fake-answer-repository.ts
@@ -1,6 +1,12 @@
 import { vi } from 'vitest'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 
+const create = vi.fn()
+
 export const fakeAnswersRepository: AnswersRepository = {
-  create: vi.fn(),
+  create,
+}
+
+export const functions = {
+  create,
 }
