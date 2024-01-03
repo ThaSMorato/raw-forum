47e4faacc0310a06447486c6a4e2542a8fe33cfe
ThaSMorato <tha.s.morato@gmail.com>
2024-01-03 01:16:19 -0300

    feat: receiving repository

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer"
+import { AnswerRepository } from "../repositories/answers-repository"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -7,8 +8,13 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  execute({instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
+
+  constructor(private answersRepository: AnswerRepository){}
+
+  async execute({instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
     const answer = new Answer({content, authorId: instructorId, questionId})
+
+    await this.answersRepository.create(answer)
 
     return answer
   }
