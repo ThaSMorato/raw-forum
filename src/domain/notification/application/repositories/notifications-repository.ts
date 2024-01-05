79dac49935eb0a680851e6436245eeacbb13a9be
ThaSMorato <tha.s.morato@gmail.com>
2024-01-05 14:26:39 -0300

    refact: typos

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../../enterprise/entities/notification'
 
 export interface NotificationsRepository {
   create(notification: Notification): Promise<void>
+  findById(notificationId: string): Promise<Notification | null>
+  save(notification: Notification): Promise<void>
 }
